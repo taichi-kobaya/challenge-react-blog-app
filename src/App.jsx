20644b4a3825decaab90e06fb262a8a42a272838
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2025-04-11 20:10:55 +0900

    チャレンジ#5：記事クリックで詳細ページ #5 - react-router-dom を導入 - 記事をクリックすると記事IDに応じたURLに遷移 - 詳細ページは仮のプレースホルダー表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import { articles } from "./articles";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
-      <h1>記事一覧</h1>
-      <ul>
-        {articles.map(article => (
-          <li key={article.id}>{article.title}</li>
-        ))}
-      </ul>
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <ul>
+          {articles.map(article => (
+            <li key={article.id}>
+              <Link to={`/articles/${article.id}`}>{article.title}</Link>
+            </li>
+          ))}
+        </ul>
+      } />
+      <Route path="/articles/:id" element={<p>詳細ページ（仮）</p>} />
+    </Routes>
   );
 }
 
