1a3a28e4a5aa9368ca9dc78d7dd4848a75ddd456
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2025-04-11 18:00:29 +0900

    チャレンジ#4：TailwindCSSの導入 #4 - TailwindCSS をインストールして設定ファイルを追加 - App コンポーネントにクラスを適用して動作確認 - index.css に Tailwind ディレクティブ追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { articles } from "./articles";
 function App() {
   return (
     <div>
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
       <h1>記事一覧</h1>
       <ul>
         {articles.map(article => (
